23a317cf78950fdea9b4295711f508a282dcf973
WoobinHwang <dnqls0326@naver.com>
2023-12-29 18:29:50 +0900

    board, comment entity commit

---
+++ omok/src/main/java/com/osite/omok/Entity/Board.java
@@ -0,0 +1,39 @@
+package com.osite.omok.Entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Board {
+
+	// 인식 번호
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer boardNum;
+	
+	// 제목
+	@Column(length = 30)
+	private String title;
+	
+	// 본문
+	@Column(columnDefinition = "TEXT")
+	private String text;
+	
+	// 작성자
+//	@ManyToOne
+//	private 유저 유저;
+
+	// 작성일시
+	private LocalDateTime writeDateTime;
+	
+}
---
+++ omok/src/main/java/com/osite/omok/Entity/comment.java
@@ -0,0 +1,39 @@
+package com.osite.omok.Entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class comment {
+
+	// 댓글 인식 번호
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer commentNum;
+	
+	// 작성자
+//	@ManyToOne
+//	private 유저 유저;
+	
+	// 본문 번호
+	@ManyToOne
+	private Board boardNum;
+	
+	// 댓글 내용
+	@Column(columnDefinition = "TEXT")
+	private String comment;
+	
+	// 작성일시
+	private LocalDateTime writeDateTime;
+	
+}
